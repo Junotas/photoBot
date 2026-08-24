3677bc548bc51b441a0cb504c9a8443b6f23ac2c
Ludvig Fendert <l.fendert@gmail.com>
2026-08-24 09:42:48 +0200

    added appProperties

---
+++ src/main/java/com/photobot/config/AppProperties.java
@@ -0,0 +1,9 @@
+package com.photobot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.nio.file.Path;
+
+@ConfigurationProperties(prefix = "app")
+public record AppProperties(
+    String discordToken, long channelId, Path photosRoot, String currentWeek) {}
